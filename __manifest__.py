2e7fe2b12905dbd9176a1fd1820da93a6ab9732e
Esteban <marketing@cerealesandinos.com>
2018-01-26 16:25:41 -0500

    Added folders
    Added basic structure

---
+++ __manifest__.py
@@ -0,0 +1,11 @@
+{
+  'name':'CA theme',
+  'description': 'CSS to adapt Loftspace',
+  'version':'1.0',
+  'author':'Esteban',
+
+  'data': [
+  ],
+  'category': 'Theme/Creative',
+  'depends': ['website', 'sale'],
+}
