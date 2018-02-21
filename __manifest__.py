48b27174f13e3af5ef491487bd354610dcb1abf8
Esteban <marketing@cerealesandinos.com>
2018-02-20 22:58:02 -0500

    FIX
    1.4

--- __manifest__.py
+++ __manifest__.py
@@ -1,15 +1,13 @@
 {
   'name':'CA Theme',
   'description': 'LESS to adapt Loftspace Theme and Beauty Theme applying CA Corporate style',
-  'version':'1.3',
+  'version':'1.4',
   'author':'Esteban Tapia',
 
   'data': [
   'views/assets.xml',
-  'views/theme_loftspace_sale.products_categories.xml'
   ],
 
-  'css': ['static/src/css/css.css'],
   'less': ['static/src/less/less.less'],
 
   'category': 'Generic Modules/Others',
